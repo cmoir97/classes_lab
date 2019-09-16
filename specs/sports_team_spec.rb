f7492bf2a22100a6688162428cf674887b39a66f
Cameron Moir <cameronjmoir@gmail.com>
2019-09-16 15:42:14 +0100

    part b complete

--- specs/sports_team_spec.rb
+++ specs/sports_team_spec.rb
@@ -5,37 +5,45 @@ require_relative("../sports_team")
 class TestSportsTeam < MiniTest::Test
 
 def test_get_team_name
-    team = SportsTeam.new("cav fc", ["Cameron", "Calum", "Sam"], "Joe" )
+    team = SportsTeam.new("cav fc", ["Cameron", "Calum", "Sam"], "Joe", 0)
     assert_equal("cav fc", team.team_name())
 end
 
 def test_get_players
-    team = SportsTeam.new("cav fc", ["Cameron", "Calum", "Sam"], "Joe" )
+    team = SportsTeam.new("cav fc", ["Cameron", "Calum", "Sam"], "Joe", 0)
     assert_equal(["Cameron", "Calum", "Sam"], team.players())
 end
 
 def test_get_coach
-    team = SportsTeam.new("cav fc", ["Cameron", "Calum", "Sam"], "Joe" )
+    team = SportsTeam.new("cav fc", ["Cameron", "Calum", "Sam"], "Joe", 0)
     assert_equal("Joe", team.coach())
 end
 
 def test_set_coach_name
-    team = SportsTeam.new("cav fc", ["Cameron", "Calum", "Sam"], "Joe" )
+    team = SportsTeam.new("cav fc", ["Cameron", "Calum", "Sam"], "Joe", 0)
     team.coach = "Jack"
     assert_equal("Jack", team.coach())
 end
 
 def test_add_player
-    team = SportsTeam.new("cav fc", ["Cameron", "Calum", "Sam"], "Joe" )
+    team = SportsTeam.new("cav fc", ["Cameron", "Calum", "Sam"], "Joe", 0)
     team.add_player("David")
     assert_equal(["Cameron", "Calum", "Sam", "David"], team.players())
 end
 
-def test_find_player
-    team = SportsTeam.new("cav fc", ["Cameron", "Calum", "Sam"], "Joe")
+def test_find_player__true
+    team = SportsTeam.new("cav fc", ["Cameron", "Calum", "Sam"], "Joe", 0)
     assert_equal("Cameron", team.find_player("Cameron"))
 end
 
+def test_find_player__false
+    team = SportsTeam.new("cav fc", ["Cameron", "Calum", "Sam"], "Joe", 0)
+    assert_equal("Not in team", team.find_player("Alan"))
+end
 
+def test_add_points_for_win
+    team = SportsTeam.new("cav fc", ["Cameron", "Calum", "Sam"], "Joe", 0)
+    assert_equal(3, team.add_points_for_win("win"))
+end
 
 end
--- sports_team.rb
+++ sports_team.rb
@@ -1,11 +1,12 @@
 class SportsTeam
 
-attr_accessor(:team_name, :players, :coach)
+attr_accessor(:team_name, :players, :coach, :points)
 
-def initialize(team_name, players, coach)
+def initialize(team_name, players, coach, points)
   @team_name = team_name
   @players = players
   @coach = coach
+  @points = points
 end
 
 def add_player(new_player)
@@ -16,9 +17,18 @@ def find_player(player_name)
     for player in @players
         if player == player_name
           return player_name
+        else
+        return "Not in team"
         end
     end
 end
+
+def add_points_for_win(result)
+    if result == "win"
+      @points += 3
+    end
+end
+
 
 
 end
